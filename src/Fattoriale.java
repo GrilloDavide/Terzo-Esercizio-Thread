1b76c55a4885fdc86a8f4e2195cbd775bab38e23
GrilloDavide <grillo.davide@itismeucci.com>
2023-04-03 17:26:05 +0200

    Piccoli aggiustamenti del progetto generale

--- src/Fattoriale.java
+++ src/Fattoriale.java
@@ -1,2 +1,19 @@
-public class Fattoriale {
+public class Fattoriale extends Thread{
+
+    private int val;
+
+    public Fattoriale(int val){
+        this.val=val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    @Override
+    public void run() {
+        int n = val;
+        for(int i = 1; i<n; val*=i, i++);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+//Davide Grillo 4AIA
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("CALCOLO DI UNA COMBINAZIONE C(n,k)");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserire n");
+        int n = input.nextInt();
+        System.out.println("Inserire k");
+        int k = input.nextInt();
+        Fattoriale f1 = new Fattoriale(n);
+        Fattoriale f2 = new Fattoriale(k);
+        Fattoriale f3 = new Fattoriale(n-k);
+        f1.start();
+        f2.start();
+        f3.start();
+        f1.join();
+        f2.join();
+        f3.join();
+        System.out.println("La combinazione C(n,k) con i valori inseriti equivale a: "+(f1.getVal()/(f2.getVal()*f3.getVal())));
     }
 }
